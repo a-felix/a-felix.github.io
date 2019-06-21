f95c5ca3c628678e834346187f9d65a7f1180604
a-felix <andreyyshore@gmail.com>
2019-06-21 20:54:18 +0300

    More useful stuff

---
+++ esc-sort/2019f/entries.js
@@ -0,0 +1,29 @@
+window.defineEntries = function (addEntry) {
+	yr = "2019f";
+	addEntry("al", 1, "Albania", "Jonida Maliqi", "Ktheju tokës", "Return to the land");
+	addEntry("au", 2, "Australia", "Kate Miller-Heidke", "Zero Gravity");
+	addEntry("az", 2, "Azerbaijan", "Chingiz", "Truth");
+	addEntry("by", 2, "Belarus", "ZENA", "Like It");
+	addEntry("cy", 1, "Cyprus", "Tamta", "Replay");
+	addEntry("cz", 1, "Czech Republic", "Lake Malawi", "Friend of a Friend");
+	addEntry("dk", 1, "Denmark", "Leonora", "Love Is Forever");
+	addEntry("ee", 2, "Estonia", "Victor Crone", "Storm");
+	addEntry("fr", 2, "France", "Bilal Hassani", "Roi", "King");
+	addEntry("de", 1, "Germany", "S!sters", "Sister");
+	addEntry("gr", 1, "Greece", "Katerine Duska", "Better Love");
+	addEntry("is", 2, "Iceland", "Hatari", "Hatrið mun sigra", "Hate will prevail");
+	addEntry("il", -14, "Israel", "Kobi Marimi", "Home");
+	addEntry("it", 2, "Italy", "Mahmood", "Soldi", "Money");
+	addEntry("mt", 1, "Malta", "Michela", "Chameleon");
+	addEntry("nl", 1, "Netherlands", "Duncan Laurence", "Arcade");
+	addEntry("mk", 1, "North Macedonia", "Tamara Todevska", "Proud");
+	addEntry("no", 2, "Norway", "KEiiNO", "Spirit in the Sky");
+	addEntry("ru", 1, "Russia", "Sergey Lazarev", "Scream");
+	addEntry("sm", 1, "San Marino", "Serhat", "Say Na Na Na");
+	addEntry("rs", 2, "Serbia", "Nevena Božović", "Kruna", "\"Круна\"; Crown");
+	addEntry("si", 1, "Slovenia", "Zala Kralj & Gašper Šantl", "Sebi", "Oneself");
+	addEntry("es", 2, "Spain", "Miki", "La venda", "The blindfold");
+	addEntry("se", 1, "Sweden", "John Lundvik", "Too Late for Love");
+	addEntry("ch", 2, "Switzerland", "Luca Hänni", "She Got Me");
+	addEntry("gb", 2, "United Kingdom", "Michael Rice", "Bigger than Us");
+}
