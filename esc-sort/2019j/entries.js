89c78fd23ff2309469e5b05a15a14bb4a549777a
a-felix <andreyyshore@gmail.com>
2019-11-14 19:09:07 +0200

    Add JESC 2019

---
+++ esc-sort/2019j/entries.js
@@ -0,0 +1,22 @@
+window.defineEntries = function (addEntry) {
+	yr = "2019j";
+	addEntry("wa", 0, "Wales", "Erin Mai", "Calon yn Curo", "Heart beating");
+	addEntry("ua", 0, "Ukraine", "Sophia Ivanko", "The Spirit of Music", "");
+	addEntry("nl", 0, "Netherlands", "Matheu", "Dans met jou", "Dance with you");
+	addEntry("es", 0, "Spain", "Melani García", "Marte", "Mars");
+	addEntry("rs", 0, "Serbia", "Darija Vračević", "Podigni glas", "\"Подигни глас\"; Raise your voice");
+	addEntry("ru", 0, "Russia", "Tatyana Mezhentseva & Denberel Oorzhak", "A Time for Us", "");
+	addEntry("pt", 0, "Portugal", "Joana Almeida", "Vem comigo", "Come with me");
+	addEntry("pl", 0, "Poland", "Viki Gabor", "Superhero", "");
+	addEntry("mk", 0, "North Macedonia", "Mila Moskov", "Fire", "");
+	addEntry("mt", 0, "Malta", "Eliana Gomez Blanco", "We Are More", "");
+	addEntry("kz", 0, "Kazakhstan", "Yerzhan Maksim", "Armanyńnan qalma", "\"Арманыңнан қалма\"; Don't waste your dreams");
+	addEntry("it", 0, "Italy", "Marta Viola", "La voce della terra", "The voice of the earth");
+	addEntry("ie", 0, "Ireland", "Anna Kearney", "Banshee", "");
+	addEntry("ge", 0, "Georgia", "Giorgi Rostiashvili", "We Need Love", "");
+	addEntry("fr", 0, "France", "Carla", "Bim bam toi", "Bim bam you");
+	addEntry("by", 0, "Belarus", "Liza Misnikova", "Пепельный", "\"Pepelny\"; Ashen");
+	addEntry("au", 0, "Australia", "Jordan Anthony", "We Will Rise", "");
+	addEntry("am", 0, "Armenia", "Karina Ignatyan", "Colours of Your Dream", "");
+	addEntry("al", 0, "Albania", "Isea Çili", "Mikja ime fëmijëri", "My childhood friend");
+}
