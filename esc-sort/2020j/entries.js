655aeec82ffe2791b9f62765ac15eeeefa728fae
a-felix <andreyyshore@gmail.com>
2020-11-10 20:58:34 +0200

    added JESC 2020

---
+++ esc-sort/2020j/entries.js
@@ -0,0 +1,15 @@
+window.defineEntries = function (addEntry) {
+	yr = "2020j";
+	addEntry("by", 0, "Belarus", "Arina Pehtereva", "Aliens", "");
+	addEntry("fr", 0, "France", "Valentina", "J'imagine", "I imagine");
+	addEntry("ge", 0, "Georgia", "Sandra Gadelia", "You Are Not Alone", "");
+	addEntry("de", 0, "Germany", "Susan", "Stronger with You", "");
+	addEntry("kz", 0, "Kazakhstan", "Karakat Bashanova", "Forever", "");
+	addEntry("mt", 0, "Malta", "Chanel Monseigneur", "Chasing Sunsets", "");
+	addEntry("nl", 0, "Netherlands", "Unity", "Best Friends", "");
+	addEntry("pl", 0, "Poland", "Ala Tracz", "I'll Be Standing", "");
+	addEntry("ru", 0, "Russia", "Sofia Feskova", "My New Day", "");
+	addEntry("rs", 0, "Serbia", "Petar Aničić", "Heartbeat", "");
+	addEntry("es", 0, "Spain", "Soleá", "Palante", "Forward");
+	addEntry("ua", 0, "Ukraine", "Oleksandr Balabanov", "Vidkryvai", "\"Відкривай\"; Open up");
+}
